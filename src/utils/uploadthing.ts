ef491ccda8a6aa00809c9291f5cdd520db5d8a55
Monkarek <gabriel.jung28@gmail.com>
2025-08-29 11:25:42 +0200

    [UPDATE] File Key

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -12,7 +12,7 @@ export const UPLOADTHING_URLS: Record<string, string> = {
 	'nexor-logo.svg': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwyv8wut95J4FTnDMxg1hCet7Sb3IrzQHp0Aw9X',
 	'Nexor-text.png': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwyTadJCW9K0M1DSYUXjuR6ygsWkt9eovb3GqmA',
 	'FullLogo-01.svg': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwyMgoZEdaw12SrxvWhQoucZRUPJCFa9ntiN8X5',
-	'pt1duo software.png': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwyV25eqPMAgLfOSseXrnk14DC6pwxGBaomi2yb',
+	'pt1duo software.png': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwy6J3xSvBs2JqRMCc5ygajmFu0Oki1DYWUbdoE',
 	'Armen_Chulyakov.png': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwye3u8obkOh3Y6H9Zi8vfcJNALEun0KRklg4j5',
 	'placeholder.svg': 'https://oybnx5jyol.ufs.sh/f/AeQfLJScGDwyq226yBbwx8aVno9PmLBqKClSF6X1RM0OAbQh',
 };
