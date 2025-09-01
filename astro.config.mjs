8e73feaf9884abc3f78d7a3b087b42db3a572b2b
Monkarek <gabriel.jung28@gmail.com>
2025-09-01 10:37:07 +0200

    [ADD] URL for dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,44 +6,93 @@ import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	site: 'https://nexor-software.netlify.app', // Update this with your actual domain
-	i18n: {
-		defaultLocale: 'de',
-		locales: ['de', 'en'],
-		routing: {
-			prefixDefaultLocale: true,
-		},
+	experimental: {
+		multiSite: true,
 	},
-	redirects: {
-		'/': '/de',
-		'/about': '/de/about',
-		'/services': '/de/services',
-		'/blog': '/de/blog',
-		'/portfolio': '/de/portfolio',
-		'/contact': '/de/contact',
-		'/privacy-policy': '/de/privacy-policy',
-		'/terms-of-service': '/de/terms-of-service',
-	},
-	integrations: [
-		react(),
-		mdx(),
-		sitemap({
+	sites: [
+		{
+			output: 'static',
+			site: 'https://nexor-software.netlify.app', // Update this with your actual domain
 			i18n: {
 				defaultLocale: 'de',
-				locales: {
-					de: 'de-DE',
-					en: 'en-US',
+				locales: ['de', 'en'],
+				routing: {
+					prefixDefaultLocale: true,
 				},
 			},
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(process.cwd(), './src'),
+			redirects: {
+				'/': '/de',
+				'/about': '/de/about',
+				'/services': '/de/services',
+				'/blog': '/de/blog',
+				'/portfolio': '/de/portfolio',
+				'/contact': '/de/contact',
+				'/privacy-policy': '/de/privacy-policy',
+				'/terms-of-service': '/de/terms-of-service',
+			},
+			integrations: [
+				react(),
+				mdx(),
+				sitemap({
+					i18n: {
+						defaultLocale: 'de',
+						locales: {
+							de: 'de-DE',
+							en: 'en-US',
+						},
+					},
+				}),
+			],
+			resolve: {
+				alias: {
+					'@': path.resolve(process.cwd(), './src'),
+				},
+			},
+			image: {
+				domains: ['oybnx5jyol.ufs.sh'],
+			},
 		},
-	},
-	image: {
-		domains: ['oybnx5jyol.ufs.sh'],
-	},
+		{
+			output: 'static',
+			site: 'https://devserver-develop--nexor-software.netlify.app/', // Replace with your new base URL
+			i18n: {
+				defaultLocale: 'de',
+				locales: ['de', 'en'],
+				routing: {
+					prefixDefaultLocale: true,
+				},
+			},
+			redirects: {
+				'/': '/de',
+				'/about': '/de/about',
+				'/services': '/de/services',
+				'/blog': '/de/blog',
+				'/portfolio': '/de/portfolio',
+				'/contact': '/de/contact',
+				'/privacy-policy': '/de/privacy-policy',
+				'/terms-of-service': '/de/terms-of-service',
+			},
+			integrations: [
+				react(),
+				mdx(),
+				sitemap({
+					i18n: {
+						defaultLocale: 'de',
+						locales: {
+							de: 'de-DE',
+							en: 'en-US',
+						},
+					},
+				}),
+			],
+			resolve: {
+				alias: {
+					'@': path.resolve(process.cwd(), './src'),
+				},
+			},
+			image: {
+				domains: ['oybnx5jyol.ufs.sh'],
+			},
+		},
+	],
 });
